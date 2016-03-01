7d69e1f7002d5e33d24cfcee4802a813e263b5f2
Lothar Braun <code@lobraun.de>
2016-03-01 13:18:16 +0100

    Extend PATH variable to include homebrew and tex

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;; extend EMACSPATH
+(setenv "BREW_BIN" "/usr/local/bin/")
+(setenv "BREW_SBIN" "/usr/local/sbin/")
+(setenv "PATH" (concat (getenv "PATH") (getenv "BREW_BIN") (getenv "BREW_SBIN") ":/Users/braun/bin/:/usr/local/texlive/2014/bin/x86_64-darwin/:"))
+
 ;; common configurations 
 (load "~/.emacs.d/my-loadpackages.el")
 (load "~/.emacs.d/compilation.el")
@@ -5,6 +10,5 @@
 (load "~/.emacs.d/user-defined-global-settings.el")
 (load "~/.emacs.d/spell-check.el")
 (load "~/.emacs.d/my-python-config.el")
-
 
 ;; (load "~/.emacs.d/my-erlang-config.el")
