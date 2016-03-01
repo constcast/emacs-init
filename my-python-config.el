0f1c0679db1c2d7113db911fed365fb51b20cbfc
Lothar Braun <code@lobraun.de>
2016-03-01 14:10:02 +0100

    Use dracula theme as default theme

--- my-python-config.el
+++ my-python-config.el
@@ -1,12 +1,25 @@
-; setup ipython
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;; ; setup ipython
+;; (setq
+;;  python-shell-interpreter "ipython"
+;;  python-shell-interpreter-args ""
+;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;  python-shell-completion-setup-code
+;;    "from IPython.core.completerlib import module_completion"
+;;  python-shell-completion-module-string-code
+;;    "';'.join(module_completion('''%s'''))\n"
+;;  python-shell-completion-string-code
+;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(elpy-enable)
+
+;; use ipython from specific path (e.g. when not in $PATH)
+;; if it exists. otherwise assume that ipython is in $PATH
+(cond
+ (
+  (file-exists-p "/usr/local/bin/ipython")
+  (elpy-use-ipython "/usr/local/bin/ipython")
+  )
+ (t
+  (elpy-use-ipython)
+  ))
--- user-defined-global-settings.el
+++ user-defined-global-settings.el
@@ -13,12 +13,16 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; color theme settings, either by package or manual selection
-(setq color-theme-selection "zenburn")
+(setq color-theme-selection "dracula")
 (cond ((equal color-theme-selection "blue")
        (load-theme 'deeper-blue)
        )
       ((equal color-theme-selection "zenburn")
        (load-theme 'zenburn t)
+       )
+      ((equal color-theme-selection "dracula")
+       (require 'dracula-theme)
+       (load-theme 'dracula t)
        )
       (t
        ;; set background and foreground colors
